42f359771e82f0207e0de85e4dadfdec0a6f74a0
Tomiwa97 <ajayitomiwa71@gmail.com>
2020-06-08 14:59:13 +0100

    File Updates

--- scripts/chinonsoHE-HNG-05667.py
+++ scripts/chinonsoHE-HNG-05667.py
@@ -1,8 +1,10 @@
 # Stage 2 Entry Task 
 
-def runTaskOne():
-    return print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format("Chinonso Egejuru", "HNG-05667", "echenri@gmail.com", "Python"))
+# def runTaskOne():
+#     return print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format("Chinonso Egejuru", #"HNG-05667", "echenri@gmail.com", "Python"))
     
 
-if __name__ == "__main__":
-    runTaskOne()
+# if __name__ == "__main__":
+#     runTaskOne()
+
+print ("Hello World, this is Chinonso Egejuru with HNGi7 ID HNG-05667 and email echenri@gmail.com using Python for stage 2 task")
--- scripts/chiomaike-HNG-04972.py
+++ scripts/chiomaike-HNG-04972.py
@@ -1,7 +1,9 @@
-name = "Chioma Ike"
-hngi7_id = "HNG-04972"
-language = "Python"
-email = "ikechioma.l@gmail.com"
+# name = "Chioma Ike"
+# hngi7_id = "HNG-04972"
+# language = "Python"
+# email = "ikechioma.l@gmail.com"
 
 
-print(f"Hello World, this is {name} with HNGi7 ID {hngi7_id} and email {email} using {language} for stage 2 task")
+# print(f"Hello World, this is {name} with HNGi7 ID {hngi7_id} and email {email} using {language} for stage 2 task")
+
+print ("Hello World, this is Chioma Ike with HNGi7 ID HNG-04972 and email ikechioma.l@gmail.com using Python for stage 2 task")
